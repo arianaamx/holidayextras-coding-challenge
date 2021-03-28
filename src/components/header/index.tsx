71e339511d81dd5c308c2c5b8d50e592e4d593ce
Ariana <ariana.milasincic@gmail.com>
2021-03-28 02:11:30 +0100

    Add Search to Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import styled from "styled-components";
 import * as colors from "../../colors";
 
-function Header() {
+interface HeaderProps {
+  setSearchByTag: Dispatch<SetStateAction<string | null>>;
+}
+
+function Header({ setSearchByTag }: HeaderProps) {
   return (
     <HeaderContainer>
       <Title>
         <h2>Flickr Photo Stream</h2>
+        <SearchInput
+          type="text"
+          placeholder="Search by tags..."
+          onChange={(event) => {
+            setSearchByTag(event.target.value);
+          }}
+        />
       </Title>
     </HeaderContainer>
   );
@@ -17,6 +28,7 @@ export default Header;
 const HeaderContainer = styled.div`
   width: 100%;
   background-color: white;
+  line-height: 3em;
 
   -webkit-box-shadow: 0px 1px 1px ${colors.LightGrey};
   -moz-box-shadow: 0px 1px 1px ${colors.LightGrey};
@@ -31,4 +43,16 @@ const HeaderContainer = styled.div`
 const Title = styled.div`
   margin: 5px 20px;
   max-width: 1200px;
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const SearchInput = styled.input`
+  padding: 5px 10px;
+  border-radius: 5px;
+  border: 3px solid ${colors.Blue};
 `;
