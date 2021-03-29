d3b257acb17bdfc4f7760a6d13c0ae93041a84a7
Ariana <ariana.milasincic@gmail.com>
2021-03-29 02:55:33 +0100

    Add Infinite Scroll

--- src/components/images-wrapper/index.tsx
+++ src/components/images-wrapper/index.tsx
@@ -4,13 +4,15 @@ import * as colors from "../../colors";
 
 import ImageContainer from "../image-container";
 import { GetPublicPhotos, Item } from "../../fetcher";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface ImagesWrapperProps {
   searchByTag: string | null;
 }
 
 function ImagesWrapper({ searchByTag }: ImagesWrapperProps) {
-  const [fetchedImages, setFetchedImages] = useState<undefined | Item[]>(undefined);
+  const [fetchedImages, setFetchedImages] = useState<Item[]>([]);
+  const [hasMore, setHasMore] = useState<boolean>(true);
 
   useEffect(() => {
     GetPublicPhotos({ setFetchedImages });
@@ -22,22 +24,37 @@ function ImagesWrapper({ searchByTag }: ImagesWrapperProps) {
 
   return (
     <ImagesContainer>
-      <Images>
-        {fetchedImages !== undefined &&
-          fetchedImages.map((element, index) => {
-            return (
-              <ImageContainer
-                key={index}
-                title={element.title}
-                description={element.description}
-                author={element.author}
-                link={element.link}
-                tags={element.tags}
-                media={element.media.m}
-              />
-            );
-          })}
-      </Images>
+      <InfiniteScroll
+        dataLength={fetchedImages!.length}
+        hasMore={hasMore}
+        height="90vh"
+        next={() => {
+          GetPublicPhotos({ setFetchedImages, fetchedImages, setHasMore });
+        }}
+        loader={<h2>Loading more images...</h2>}
+        endMessage={
+          <h3 style={{ textAlign: "center", marginTop: "20px" }}>
+            <b>You have seen all popular photos!</b>
+          </h3>
+        }
+      >
+        <Images>
+          {fetchedImages !== undefined &&
+            fetchedImages.map((element, index) => {
+              return (
+                <ImageContainer
+                  key={index}
+                  title={element.title}
+                  description={element.description}
+                  author={element.author}
+                  link={element.link}
+                  tags={element.tags}
+                  media={element.media.m}
+                />
+              );
+            })}
+        </Images>
+      </InfiniteScroll>
     </ImagesContainer>
   );
 }
@@ -51,14 +68,24 @@ const ImagesContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
+  align-items: center;
   padding: 20px 20px;
 `;
 
 const Images = styled.div`
   max-width: 1200px;
   column-count: 4;
+  text-align: center;
 
   @media (max-width: 1300px) {
     columns: 3;
+  }
+
+  @media (max-width: 950px) {
+    columns: 2;
+  }
+
+  @media (max-width: 650px) {
+    columns: 1;
   }
 `;
