6e3a5c55b5fcee44cf82124bad0fc38d0a34e631
Ariana <ariana.milasincic@gmail.com>
2021-03-29 02:54:20 +0100

    Add fetching new images, and setting the hasMore attribute

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -13,18 +13,48 @@ export type Item = {
   media: { m: string };
 };
 
-interface GetPublicPhotosProps {
+type GetPublicPhotosProps = {
   setFetchedImages: Function;
   searchByTag?: string | null;
-}
+  fetchedImages?: Item[];
+  setHasMore?: Function;
+};
 
-export const GetPublicPhotos = ({ setFetchedImages, searchByTag }: GetPublicPhotosProps) => {
-  jsonp(URL + `/feeds/photos_public.gne?format=json&tags=${searchByTag}`, { name: "jsonFlickrFeed" }, (err, data) => {
+type AddNewFetchedImagesProps = {
+  oldImages: Item[];
+  newImages: Item[];
+  setHasMore: Function | undefined;
+};
+
+export const GetPublicPhotos = ({ setFetchedImages, searchByTag, fetchedImages, setHasMore }: GetPublicPhotosProps) => {
+  const addNewFetchedImages = ({ oldImages, newImages, setHasMore }: AddNewFetchedImagesProps) => {
+    let newList = oldImages.slice(0);
+
+    // Find values that are in new fetched images but not in the old ones
+    let uniqueResults = newImages.filter(function (obj) {
+      return !oldImages.some(function (obj2) {
+        return obj.description === obj2.description;
+      });
+    });
+
+    if (uniqueResults.length === 0 && setHasMore !== undefined) {
+      setHasMore(false);
+    }
+
+    return newList.concat(uniqueResults);
+  };
+
+  const tag = searchByTag ? searchByTag : "";
+
+  jsonp(URL + `/feeds/photos_public.gne?format=json&tags=${tag}`, { name: "jsonFlickrFeed" }, (err, data) => {
     if (err) {
       console.log(err);
     } else {
-      // console.log(data);
-      setFetchedImages(data.items);
+      let items = data.items;
+      if (fetchedImages) {
+        items = addNewFetchedImages({ oldImages: fetchedImages, newImages: data.items, setHasMore: setHasMore });
+      }
+      setFetchedImages(items);
     }
   });
 };
